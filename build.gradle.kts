7c85d4503ae07117a12ce78e5bef1ebfc653d2de
ho8278 <ho8278@naver.com>
2024-03-11 21:57:04 +0900

    Retrofit, Coroutine, Serialize 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,7 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.android.library") version "8.1.1" apply false
+
+    kotlin("jvm") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.0"
 }
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -3,6 +3,7 @@ import org.gradle.initialization.Environment.Properties
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    kotlin("plugin.serialization")
 }
 
 android {
@@ -44,6 +45,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
