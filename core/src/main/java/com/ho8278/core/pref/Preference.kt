c4d9ddf40222d5c33700b220f784f40e92e98aac
ho8278 <ho8278@naver.com>
2024-03-13 21:28:12 +0900

    DomainPreference -> Preference 로 네이밍 변경

--- core/src/main/java/com/ho8278/core/pref/Preference.kt
+++ core/src/main/java/com/ho8278/core/pref/Preference.kt
@@ -2,7 +2,7 @@ package com.ho8278.core.pref
 
 import java.lang.reflect.Type
 
-interface DomainPreference {
+interface Preference {
     suspend fun <T> getValue(domainName: String, key: String, type: Type): T?
     suspend fun <T> putValue(domainName: String, key: String, value: T, type: Type)
     suspend fun removeValue(domainName: String, key: String)
