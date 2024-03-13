124c264a3050659a296fdfdd6c1da849cc36a023
ho8278 <ho8278@naver.com>
2024-03-13 21:30:41 +0900

    도메인을 이미 알고 있는 preference 클래스 추가

---
+++ core/src/main/java/com/ho8278/core/pref/DomainPreference.kt
@@ -0,0 +1,24 @@
+package com.ho8278.core.pref
+
+import java.lang.reflect.Type
+
+class DomainPreference(
+    private val preference: Preference,
+    private val domain: String
+) {
+    suspend fun <T> getValue(key: String, type: Type): T? {
+        return preference.getValue(domain, key, type)
+    }
+
+    suspend fun <T> putValue(key: String, value: T, type: Type) {
+        preference.putValue(domain, key, value, type)
+    }
+
+    suspend fun removeValue(key: String) {
+        preference.removeValue(domain, key)
+    }
+
+    suspend fun clear() {
+        preference.clear(domain)
+    }
+}
