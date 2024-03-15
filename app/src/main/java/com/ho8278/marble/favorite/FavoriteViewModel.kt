849079b22c6314e938c4be1e3b2cb33e9d7817d7
ho8278 <ho8278@naver.com>
2024-03-16 02:25:23 +0900

    itemList 프로퍼티 추가하기

--- app/src/main/java/com/ho8278/marble/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/ho8278/marble/favorite/FavoriteViewModel.kt
@@ -2,8 +2,11 @@ package com.ho8278.marble.favorite
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ho8278.data.model.Card
 import com.ho8278.data.repository.MarbleRepository
+import com.ho8278.marble.common.ItemHolder
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -12,9 +15,17 @@ class FavoriteViewModel @Inject constructor(
     private val marbleRepository: MarbleRepository
 ) : ViewModel() {
 
-    fun onCardClick(id: Int) {
+    val itemList = marbleRepository.favoriteChanges()
+        .map { cards ->
+            println(cards.joinToString())
+            cards.map {
+                ItemHolder(it, true)
+            }
+        }
+
+    fun onCardClick(card: Card) {
         viewModelScope.launch {
-            marbleRepository.removeFavorite(id)
+            marbleRepository.removeFavorite(card.characterId)
         }
     }
 }
