20e9ea871e67911e02bd8e906eba8b7cd28fd8cd
ho8278 <ho8278@naver.com>
2024-03-16 01:40:04 +0900

    카드 삭제로직 추가

--- app/src/main/java/com/ho8278/marble/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/ho8278/marble/favorite/FavoriteViewModel.kt
@@ -1,8 +1,10 @@
 package com.ho8278.marble.favorite
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ho8278.data.repository.MarbleRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +12,9 @@ class FavoriteViewModel @Inject constructor(
     private val marbleRepository: MarbleRepository
 ) : ViewModel() {
 
+    fun onCardClick(id: Int) {
+        viewModelScope.launch {
+            marbleRepository.removeFavorite(id)
+        }
+    }
 }
