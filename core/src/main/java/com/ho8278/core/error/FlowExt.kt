daace2a61d4b56e04b503788c3e304563d975da4
ho8278 <ho8278@naver.com>
2024-03-16 00:31:24 +0900

    CancellationException 일 경우에는 재구독 하지 않기

--- core/src/main/java/com/ho8278/core/error/FlowExt.kt
+++ core/src/main/java/com/ho8278/core/error/FlowExt.kt
@@ -1,5 +1,6 @@
 package com.ho8278.core.error
 
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -9,6 +10,8 @@ fun <T> Flow<T>.stable(): Flow<T> {
         while (true) {
             try {
                 collect { emit(it) }
+            } catch (cancellationException: CancellationException) {
+                throw cancellationException
             } catch (ignore: Throwable) {
                 delay(1000L)
             }
